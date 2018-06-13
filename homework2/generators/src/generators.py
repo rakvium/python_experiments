5ddff73c970ecff487f20b0d392c6f283729a2fc
Viktor Rak <v.rak@sloboda-studio.com>
2018-06-13 15:42:50 +0300

    homework 2.2: Close file after write

--- homework2/generators/src/generators.py
+++ homework2/generators/src/generators.py
@@ -8,13 +8,15 @@ location = os.path.realpath(
 with open(location, encoding='utf-8') as data_file:
     data = json.loads(data_file.read())
 
-print(data)
-
 location = os.path.realpath(
     os.path.join(
         os.getcwd(), 'config_backup.env'))
 
 backup = open(location, 'w')
+if backup:
+    for d in data:
+        backup.write("".join(["%s=%s\n" % (k, str(v)) for k, v in d.items()]))
+    backup.close()
 
-for values in data:
-    backup.write("".join(["%s=%s\n" % (k, str(v)) for k, v in values.items()]))
+if __name__ == "__main__":
+    print(data)
