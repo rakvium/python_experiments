644837e472a10d091b067f6d76742d2e0bc836db
Viktor Rak <v.rak@sloboda-studio.com>
2018-06-13 12:23:29 +0300

    homework 2.2: Save config to file

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 - [x] 1: Python's ecosystem
 - [x] 2.1: Python: classes
-- [ ] 2.2: Python: generators
+- [x] 2.2: Python: generators
 - [ ] 3: Framework Django - part 1
 - [ ] 4: Framework Django - part 2
 - [ ] 5: Framework Flask
--- homework2/generators/src/generators.py
+++ homework2/generators/src/generators.py
@@ -9,3 +9,13 @@ with open(location, encoding='utf-8') as data_file:
     data = json.loads(data_file.read())
 
 print(data)
+
+location = os.path.realpath(
+    os.path.join(
+        os.getcwd(), 'config_backup.env'))
+
+backup = open(location, 'w')
+
+dict_list = data
+for values in dict_list:
+    backup.write("".join(["%s=%s\n" % (k, str(v)) for k, v in values.items()]))
